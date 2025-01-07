de3eb95642d9a73723aa9177ceacdba4817f0a48
Felleria <tellytabbie@gmail.com>
2025-01-07 12:52:03 +0300

    🔧 🛠️ Update Tailwind CSS configuration to include Playfair font
    🛠️ Update Tailwind CSS configuration to include Playfair font

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-center item-center h-screen bg-white'>
+        <h1 className='class="text-5xl font-bold text-black tracking-wider font-playfair" '>Static Gallery</h1>
+    </div>
+  )
+}
+
+export default Header
