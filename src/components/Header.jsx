961b15ae64b79c9651731b71adff9056459ed21a
Felleria <tellytabbie@gmail.com>
2025-01-14 01:59:56 +0300

    🎨 Improve structure
    Improve structure of code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { FaHome, FaSearch, FaShoppingBag } from 'react-icons/fa';
+import { FaHome, FaShoppingBag } from 'react-icons/fa';
 
 const Header = () => {
   return (
     <header className="bg-white text-black fixed top-0 left-0 w-full z-50 shadow-md flex items-center justify-between px-6 py-4">
-      {/* Logo */}
-      <h1 className="text-2xl font-bold font-playfair uppercase tracking-wide">Static</h1>
+      {}
+      <form className="flex items-center border border-black rounded-lg px-4 py-1">
+        <input
+          type="text"
+          placeholder="Search items"
+          className="bg-white text-sm text-black focus:outline-none placeholder-gray-500"
+        />
+      </form>
 
 
-      {/* Navigation Links */}
-      <nav className="hidden md:flex space-x-6">
+      {}
+      <h1 className="text-2xl font-bold font-playfair uppercase tracking-wide text-center">
+        Static
+      </h1>
+
+
+
+      {}
+      <nav className="flex space-x-6">
         <ul className="flex space-x-6">
           <li className="cursor-pointer hover:text-gray-500">
             <FaHome size={20} />
@@ -19,20 +32,6 @@ const Header = () => {
           </li>
         </ul>
       </nav>
-
-
-
-      {/* Search Bar */}
-      <form className="flex items-center border border-black rounded-lg px-4 py-1">
-        <input
-          type="text"
-          placeholder="Search items"
-          className="bg-white text-sm text-black focus:outline-none placeholder-gray-500"
-        />
-        <button type="submit" className="ml-2 text-black hover:text-gray-500">
-          <FaSearch />
-        </button>
-      </form>
     </header>
   );
 };
