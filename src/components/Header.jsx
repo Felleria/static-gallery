3aa11012a568c6c2ebc9a6e5a6f5246da065ff4c
Felleria <tellytabbie@gmail.com>
2025-02-19 00:15:46 +0300

    🔨 Correct import from react-icons/ri
    Correct import from react-icons/ri

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { RiAccountCircle2Line , FaShoppingBag } from 'react-icons/fa';
-
+import { RiAccountCircleLine } from 'react-icons/ri'; 
+import { FaShoppingBag } from 'react-icons/fa';
 
 const Header = () => {
   return (
     <header className="bg-white text-black fixed top-0 left-0 w-full z-50 shadow-md flex items-center justify-between px-6 py-4">
-      {}
       <form className="flex items-center border border-black rounded-lg px-4 py-1">
         <input
           type="text"
@@ -14,19 +13,14 @@ const Header = () => {
         />
       </form>
 
-
-      {}
       <h1 className="text-2xl font-bold font-playfair uppercase tracking-wide text-center">
         Static
       </h1>
 
-
-
-      {}
       <nav className="flex space-x-6">
         <ul className="flex space-x-6">
           <li className="cursor-pointer hover:text-gray-500">
-          <RiAccountCircle2Line />
+            <RiAccountCircleLine size={24} />
           </li>
           <li className="cursor-pointer hover:text-gray-500">
             <FaShoppingBag size={20} />
